a7a058c2c2a077a406f6daf70f58432f2194b79d
Pandey <ujjawal.pandey@accenture.com>
2022-11-21 15:24:29 +0530

    database-connection

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express()
 const port = 3000
+
+const config=require("./config")
+
+dbUrl=config.dbUrl;
+
+
+mongoose.connect(dbUrl, (err) => {
+    if (err) console.log(err);
+    else console.log("success")
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
---
+++ config.js
@@ -0,0 +1,5 @@
+let config={
+    dbUrl:"mongodb+srv://admin:test123@cluster0.kc0arwi.mongodb.net/ecommerce-data?retryWrites=true&w=majority"
+};
+
+module.exports=config
